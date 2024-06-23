d6194da47730616519a81d2e97afd4597cd0d1b6
Mauricio Prado <u202010482@upc.edu.pe>
2024-06-22 20:29:52 -0500

    reboot project.

--- src/main/java/com/medibox/MedicationAdherenceReminders/domain/persistence/FrequencyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.medibox.MedicationAdherenceReminders.domain.persistence;
-
-import com.medibox.MedicationAdherenceReminders.domain.model.entity.Frequency;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FrequencyRepository extends JpaRepository<Frequency, Long> {
-}
